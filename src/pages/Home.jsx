0eabb9b99073aaee6ae573d7801e245cf0fbe4ee
bm4656 <newjin46@gmail.com>
2022-12-13 20:44:49 +0900

    prettier 적용 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import tw from "tailwind-styled-components"
+import tw from 'tailwind-styled-components';
 
 function Home() {
-    return (
-        <div></div>
-    );
+  return <div></div>;
 }
 
 export default Home;
