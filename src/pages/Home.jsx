53a891338bff48441e4863007b3b6d16d9afabeb
sujeong ko <dev.sjko@gmail.com>
2022-12-13 17:24:42 +0900

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from "tailwind-styled-components"
+
+function Home() {
+    return (
+        <div></div>
+    );
+}
+
+export default Home;
