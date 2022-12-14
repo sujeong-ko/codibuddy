61b4ef6a23b3be4d47230611eb3350d5c1617a7d
sujeong ko <dev.sjko@gmail.com>
2022-12-14 17:55:56 +0900

    feat: header 작업중

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+import logo from '../assets/logo192.png';
 
 const Header = () => {
-  return <div></div>;
+  const GuestNav = () => {
+    return (
+      <>
+        <button>로그인</button>
+        <button>회원가입</button>
+      </>
+    );
+  };
+  return (
+    <MyHeader>
+      <Logo src={logo} />
+      <Nav>
+        <GuestNav />
+      </Nav>
+    </MyHeader>
+  );
 };
+
+const MyHeader = tw.header`
+h-16
+p-3
+border
+flex
+items-center
+`;
+
+const Logo = styled.img`
+  height: 3rem;
+`;
+
+const Nav = tw.nav`
+`;
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
+import Header from '../components/Header';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
