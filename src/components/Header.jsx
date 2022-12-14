7674e5b174ea6be42a42da0ef5d04a609ade098f
sujeong ko <dev.sjko@gmail.com>
2022-12-14 23:21:02 +0900

    feat: header 마크업 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 import logo from '../assets/logo192.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const goToHome = () => {
+    navigate('/');
+  };
+
   const GuestNav = () => {
     return (
       <>
-        <button>로그인</button>
-        <button>회원가입</button>
+        <Button>로그인</Button>
+        <Button>회원가입</Button>
+      </>
+    );
+  };
+
+  const MemberNav = () => {
+    return (
+      <>
+        <Button>새 글 쓰기</Button>
+        <Button>마이페이지</Button>
       </>
     );
   };
   return (
     <MyHeader>
-      <Logo src={logo} />
-      <Nav>
+      <Logo src={logo} onClick={goToHome} />
+      <nav>
         <GuestNav />
-      </Nav>
+        {/* <MemberNav /> */}
+      </nav>
     </MyHeader>
   );
 };
@@ -27,14 +43,17 @@ h-16
 p-3
 border
 flex
+justify-between
 items-center
 `;
 
 const Logo = styled.img`
   height: 3rem;
+  cursor: pointer;
 `;
 
-const Nav = tw.nav`
+const Button = tw.button`
+ml-3
 `;
 
 export default Header;
