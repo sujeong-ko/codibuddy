1ac12585383e8e42c26bf851381f71b20f1d7979
2emily <2eunbida@gmail.com>
2022-12-19 20:51:57 +0900

    함수형으로 수정

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import tw from 'tailwind-styled-components';
 import { Link } from 'react-router-dom';
 
-class Login extends Component {
+const Login = () => {
   // state = {
-  //   email: "",
-  //   password: "",
+  //   email: '',
+  //   password: '',
   // };
 
-  // loginHandler = (e) => {
+  // const loginHandler = (e) => {
   //   const { name, value } = e.target;
   //   this.setState({ [name]: value });
-  // };   ////계산된 속성명 사용
+  // };
 
-  // loginClickHandler = () => {
+  // ////계산된 속성명 사용
+
+  // const loginClickHandler = () => {
   //   const { email, password } = this.state;
-  //   fetch("http://10.58.2.17:8000/auth/login", {
-  //     method: "POST",
+  //   fetch('http://10.58.2.17:8000/auth/login', {
+  //     method: 'POST',
   //     headers: {
-  //       "Content-Type": "application/json",
+  //       'Content-Type': 'application/json',
   //     },
   //     body: JSON.stringify({
   //       email,
@@ -29,89 +31,88 @@ class Login extends Component {
   //     .then((res) => console.log(res));
   // };
 
-  render() {
-    // const { isOpen, close } = this.props;   //아까 버튼에서 props로 가져온것
-    return (
-      <>
-        {
-          // isOpen ?
-          ////만약 isopen(this.state.isModalOpen)이 true일때 코드를 실행 false면  null
-          /// <div onClick={close}> 로그인창 말고 회색 바탕을 누를시 모달이 꺼지게 만듬
-          ///<span className="close" onClick={close}>&times;</span> x버튼 누를시 꺼짐
-          ////<div className="modalContents" onClick={isOpen}> 로그인 화면은 버튼 클릭해서 들어오면
-          /// true인 상태로 있어서 화면이 안꺼진다.
+  return (
+    <>
+      {/* // isOpen ?
+    ////만약 isopen(this.state.isModalOpen)이 true일때 코드를 실행 false면  null
+    /// <div onClick={close}> 로그인창 말고 회색 바탕을 누를시 모달이 꺼지게 만듬
+    ///<span className="close" onClick={close}>&times;</span> x버튼 누를시 꺼짐
+    ////<div className="modalContents" onClick={isOpen}> 로그인 화면은 버튼 클릭해서 들어오면
+    /// true인 상태로 있어서 화면이 안꺼진다. */}
 
-          <ModalMain>
-            <div
+      <ModalMain>
+        <div
+        // onClick={close}
+        >
+          <LoginModal>
+            <Close
             // onClick={close}
             >
-              <LoginModal>
-                <Close
-                // onClick={close}
-                >
-                  &times;
-                </Close>
-                <ModalContents
-                // onClick={isOpen}
-                >
-                  <LoginTitle>로그인</LoginTitle>
-                  <TitleText>ID</TitleText>
-                  <Input
-                    name='email'
-                    className='loginId'
-                    type='text'
-                    placeholder='아이디'
-                    onChange={this.loginHandler}
+              &times;
+            </Close>
+            <ModalContents
+            // onClick={isOpen}
+            >
+              <LoginTitle>로그인</LoginTitle>
+              <TitleText>ID</TitleText>
+              <Input
+                name='email'
+                className='loginId'
+                type='text'
+                placeholder='아이디'
+                // onChange={this.loginHandler}
+              />
+              <TitleText>Password</TitleText>
+              <Input
+                name='password'
+                className='loginPw'
+                type='password'
+                placeholder='비밀번호'
+                // onChange={this.loginHandler}
+              />
+              {/* <div className='loginMid'>
+                <label className='autoLogin' htmlFor='hint'>
+                  {' '}
+                  <input type='checkbox' id='hint' /> 로그인 유지하기
+                </label>
+                <div className='autoLogin'>아이디/비밀번호 찾기</div>
+              </div> */}
+              <LoginBtn
+              // onClick={this.loginClickHandler}
+              >
+                {' '}
+                LOGIN{' '}
+              </LoginBtn>
+              {/* <div className='socialBox'>
+                <div className='kakao'>
+                  <img
+                    className='kakaoLogo'
+                    src='/Images/SignIn/kakao.png'
                   />
-                  <TitleText>Password</TitleText>
-                  <Input
-                    name='password'
-                    className='loginPw'
-                    type='password'
-                    placeholder='비밀번호'
-                    onChange={this.loginHandler}
+                  <div className='kakaoText'>카카오 계정으로 신규가입</div>
+                </div>
+                <div className='facebook'>
+                  <img
+                    className='facebookLogo'
+                    src='/Images/SignIn/facebook.png'
                   />
-                  {/* <div className='loginMid'>
-                    <label className='autoLogin' htmlFor='hint'>
-                      {' '}
-                      <input type='checkbox' id='hint' /> 로그인 유지하기
-                    </label>
-                    <div className='autoLogin'>아이디/비밀번호 찾기</div>
-                  </div> */}
-                  <LoginBtn onClick={this.loginClickHandler}> LOGIN </LoginBtn>
-                  {/* <div className='socialBox'>
-                    <div className='kakao'>
-                      <img
-                        className='kakaoLogo'
-                        src='/Images/SignIn/kakao.png'
-                      />
-                      <div className='kakaoText'>카카오 계정으로 신규가입</div>
-                    </div>
-                    <div className='facebook'>
-                      <img
-                        className='facebookLogo'
-                        src='/Images/SignIn/facebook.png'
-                      />
-                      <div className='facebookText'>
-                        페이스북 계정으로 신규가입
-                      </div>
-                    </div>
-                  </div> */}
-                  <LoginEnd>
-                    <LoginLine>
-                      회원이 아니신가요? <Link to='/register'>회원가입</Link>
-                    </LoginLine>
-                  </LoginEnd>
-                </ModalContents>
-              </LoginModal>
-            </div>
-          </ModalMain>
-          // : null
-        }
-      </>
-    );
-  }
-}
+                  <div className='facebookText'>
+                    페이스북 계정으로 신규가입
+                  </div>
+                </div>
+              </div> */}
+              <LoginEnd>
+                <LoginLine>
+                  회원이 아니신가요? <Link to='/register'>회원가입</Link>
+                </LoginLine>
+              </LoginEnd>
+            </ModalContents>
+          </LoginModal>
+        </div>
+      </ModalMain>
+    </>
+  );
+};
 
 const ModalMain = tw.div`
 fixed top-0 left-0 right-0
