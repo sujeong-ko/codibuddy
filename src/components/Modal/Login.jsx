44733e8622b98f8c56e69bc08000a7befe4028da
sujeong ko <dev.sjko@gmail.com>
2022-12-22 19:46:34 +0900

    fix: login input name 속성 삭제

--- src/components/Modal/Login.jsx
+++ src/components/Modal/Login.jsx
@@ -38,7 +38,6 @@ const Login = () => {
             <S.ModalTitle>로그인</S.ModalTitle>
             <S.TitleText>ID</S.TitleText>
             <S.Input
-              name='email'
               className='loginId'
               type='text'
               placeholder='아이디'
@@ -47,7 +46,6 @@ const Login = () => {
             />
             <S.TitleText>Password</S.TitleText>
             <S.Input
-              name='password'
               className='loginPw'
               type='password'
               placeholder='비밀번호'
