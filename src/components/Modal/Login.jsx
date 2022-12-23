bc9a9024103173d7a4b85379df71550681fa07ae
sujeong ko <dev.sjko@gmail.com>
2022-12-23 17:21:48 +0900

    login error 해결중

--- src/components/Modal/Login.jsx
+++ src/components/Modal/Login.jsx
@@ -4,53 +4,59 @@ import * as S from './Modal.style';
 import { useDispatch } from 'react-redux';
 import modalSlice from '../../redux/modalSlice.jsx';
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
-  const [loginInfo, setLoginInfo] = useState({
-    user_id: '',
-    pw: '',
-  });
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
   const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
   const close = () => {
     dispatch(modalSlice.actions.loginToggle());
   };
-  const userIdHandler = (e) =>
-    setLoginInfo({ ...loginInfo, user_id: e.target.value });
-  const pwHandler = (e) => setLoginInfo({ ...loginInfo, pw: e.target.value });
-  const submitHandler = async () => {
+
+  const onSubmit = async (data) => {
+    console.log(data);
     try {
-      const { data } = await axios.post('api/users/login', loginInfo);
-      localStorage.setItem('token', data.token);
-      console.log(data);
+      const result = await axios.post(
+        'api/users/login',
+        JSON.stringify(data),
+        config,
+      );
+      console.log(result);
+      // localStorage.setItem('token', data.token);
+      dispatch(modalSlice.actions.loginToggle());
     } catch (err) {
       console.log(err);
     }
-    dispatch(modalSlice.actions.loginToggle());
   };
+
   return (
     <>
       <S.ModalMain>
         <S.ModalDiv>
           <S.Close onClick={close}>&times;</S.Close>
-          <S.ModalContents>
+          <S.ModalContents onSubmit={handleSubmit(onSubmit)}>
             <S.ModalTitle>로그인</S.ModalTitle>
             <S.TitleText>ID</S.TitleText>
             <S.Input
               className='loginId'
               type='text'
               placeholder='아이디'
-              value={loginInfo.user_id}
-              onChange={userIdHandler}
+              {...register('user_id')}
             />
             <S.TitleText>Password</S.TitleText>
             <S.Input
               className='loginPw'
               type='password'
               placeholder='비밀번호'
-              value={loginInfo.pw}
-              onChange={pwHandler}
+              {...register('pw')}
             />
-            <S.ModalBtn onClick={submitHandler}> LOGIN </S.ModalBtn>
+            <S.ModalBtn> LOGIN </S.ModalBtn>
             <S.LoginEnd>
               <S.LoginLine
                 onClick={() => {
