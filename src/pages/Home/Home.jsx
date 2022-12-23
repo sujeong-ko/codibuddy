3b8b7f7d9419aff3a6c02fa8e3ea6f75e5e067c5
bm4656 <newjin46@gmail.com>
2022-12-23 17:07:05 +0900

    styled: home 수정 + studyCard 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import CategoryNav from '../../components/NavBar/CategoryNav';
 import LanguageNav from '../../components/NavBar/LanguageNav';
 import StudyCard from '../../components/StudyCard/StudyCard';
-import { Wrap } from './Home.styles.jsx';
+import { Wrap, StudyWrap } from './Home.styles.jsx';
 const Home = () => {
+  const dumy = [1, 2, 3, 4, 5, 6];
   return (
     <Wrap>
       <CategoryNav />
       <LanguageNav />
-      <StudyCard />
+      <StudyWrap>
+        {dumy.map((i) => (
+          <StudyCard key={i} />
+        ))}
+      </StudyWrap>
     </Wrap>
   );
 };
