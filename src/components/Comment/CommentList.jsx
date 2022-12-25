5d81aee99206da15b5f2d530e5031afc4e5ca836
sujeong ko <dev.sjko@gmail.com>
2022-12-26 02:55:46 +0900

    feat: comment POST API 작업중

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -4,6 +4,7 @@ import Comment from './Comment';
 import { CommentWrap, CommentInput } from './Comment.styles.jsx';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 // const comments = [
 //   { author: '죠리퐁', body: '저도 하고싶어여' },
@@ -11,28 +12,42 @@ import { useParams } from 'react-router-dom';
 // ];
 
 const CommentList = () => {
-  const [axiosComments, setaxiosComments] = useState([]);
+  const [comments, setComments] = useState([]);
   const { id: study_id } = useParams();
-
+  const { register, handleSubmit } = useForm();
   useEffect(() => {
     const getComments = async () => {
       await axios
         .get(`/api/comment/${study_id}`)
         .then((response) => {
-          setaxiosComments([...response.data]);
+          setComments([...response.data]);
         })
-        .catch((err) => console.log(err.response.data.reason));
+        .catch((err) => console.log(err));
     };
     getComments();
   }, []);
+  const onSubmit = async (data) => {
+    try {
+      const result = await axios.post('/api/comment', {
+        study_id,
+        commentary: data.commentary,
+      });
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <CommentWrap>
+    <CommentWrap onSubmit={handleSubmit(onSubmit)}>
       <div className='font-bold text-lg'>댓글</div>
-      <CommentInput placeholder='댓글을 작성해주세요.' />
+      <CommentInput
+        {...register('commentary')}
+        placeholder='댓글을 작성해주세요.'
+      />
       <div className='flex justify-end'>
         <Button type='basic' text='등록하기' />
       </div>
-      {axiosComments.map((item, idx) => {
+      {comments.map((item, idx) => {
         return <Comment key={idx} {...item} />;
       })}
       {/* item에는 author, content, studyId, createdAt 들어있음 */}
