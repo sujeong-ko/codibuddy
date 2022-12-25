171c882fd198776b592a0f7507b4bdbf9bc351b7
sujeong ko <dev.sjko@gmail.com>
2022-12-26 02:32:36 +0900

    feat: 댓글 GET API

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../Button/Button.jsx';
 import Comment from './Comment';
 import { CommentWrap, CommentInput } from './Comment.styles.jsx';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-const comments = [
-  { author: '죠리퐁', body: '저도 하고싶어여' },
-  { author: '버블티', body: '재밌겠다' },
-];
+// const comments = [
+//   { author: '죠리퐁', body: '저도 하고싶어여' },
+//   { author: '버블티', body: '재밌겠다' },
+// ];
 
 const CommentList = () => {
+  const [axiosComments, setaxiosComments] = useState([]);
+  const { id: study_id } = useParams();
+
+  useEffect(() => {
+    const getComments = async () => {
+      await axios
+        .get(`/api/comment/${study_id}`)
+        .then((response) => {
+          setaxiosComments([...response.data]);
+        })
+        .catch((err) => console.log(err.response.data.reason));
+    };
+    getComments();
+  }, []);
   return (
     <CommentWrap>
       <div className='font-bold text-lg'>댓글</div>
@@ -16,7 +32,7 @@ const CommentList = () => {
       <div className='flex justify-end'>
         <Button type='basic' text='등록하기' />
       </div>
-      {comments.map((item, idx) => {
+      {axiosComments.map((item, idx) => {
         return <Comment key={idx} {...item} />;
       })}
       {/* item에는 author, content, studyId, createdAt 들어있음 */}
