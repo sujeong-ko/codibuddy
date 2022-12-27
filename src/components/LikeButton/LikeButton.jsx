7b71df9293714ade88d255a28bb15fd45ead554b
sujeong ko <dev.sjko@gmail.com>
2022-12-28 00:54:53 +0900

    feat: 찜하기 기능 에러 처리 구현 중

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -10,8 +10,19 @@ const LikeButton = ({ id }) => {
       const result = await axios.post(`/api/like/${id}`, null, {
         headers: config(token),
       });
+      console.log(result);
     } catch (err) {
-      console.log(err.response);
+      if (err.response.status === 405) {
+        if (confirm('이미 찜한 모임입니다. 찜을 취소하시겠습니까?')) {
+          await axios.delete(`/api/like/${id}`, null, {
+            headers: config(token),
+          });
+        }
+        return;
+      } else if (err.response.status === 403) {
+        alert('로그인이 필요한 서비스입니다.');
+        return;
+      }
     }
   };
   return (
