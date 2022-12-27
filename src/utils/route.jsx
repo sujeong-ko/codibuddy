db2564dbcc13872dc22188b76fe6ca889866b820
sujeong ko <dev.sjko@gmail.com>
2022-12-27 23:40:50 +0900

    feat: route.jsx 추가

---
+++ src/utils/route.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {
+  Home,
+  MyPage,
+  Payment,
+  PaymentComplete,
+  StudyDetail,
+  UpdateMyPage,
+  NewStudy,
+  NotFound,
+} from '../pages';
+
+export const ROUTE = {
+  HOME: {
+    path: '/',
+    link: '/',
+    element: <Home />,
+  },
+  STUDY_PAGE: {
+    path: 'study/:id',
+    link: '/study/:id',
+    element: <StudyDetail />,
+  },
+  NEW_STUDY_PAGE: {
+    path: 'new',
+    link: '/new',
+    element: <NewStudy />,
+  },
+  MY_PAGE: {
+    path: 'mypage',
+    link: '/mypage',
+    element: <MyPage />,
+  },
+  USER_UPDATE_PAGE: {
+    path: 'update-mypage',
+    link: '/update-mypage',
+    element: <UpdateMyPage />,
+  },
+  PAYMENT: {
+    path: 'payment/:id',
+    link: '/payment/:id',
+    element: <Payment />,
+  },
+  PAYMENT_COMPLETE: {
+    path: 'payment/complete',
+    link: '/payment/complete',
+    element: <PaymentComplete />,
+  },
+  NOT_FOUND: {
+    path: '*',
+    link: '/*',
+    element: <NotFound />,
+  },
+};
+
+export const ROUTE_ARR = Object.values(ROUTE);
