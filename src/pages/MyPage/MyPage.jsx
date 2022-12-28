d69282ad59fd53cfaaa9951a22e96fca6850b786
bm4656 <newjin46@gmail.com>
2022-12-28 16:57:22 +0900

    feat: 마이페이지 api + 찜목록

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../../components/MyPage/Sidebar';
 import Profile from '../../components/MyPage/Profile';
 import StudyCard from '../../components/StudyCard/StudyCard';
 import { Wrap, Box, Item } from './MyPage.styles';
+import axios from 'axios';
+
 const MyPage = () => {
-  const dumyData = [1, 2, 3];
+  const token = localStorage.getItem('userToken');
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const [userDatas, setUserDatas] = useState();
+  const [likes, setLikes] = useState();
+  const userInfo = async () => {
+    await axios
+      .get(`/api/user`, config)
+      .then((response) => {
+        // console.log(response.data);
+        setUserDatas(response.data[0]);
+      })
+      .catch((err) => console.log(err));
+  };
+  const likeList = async () => {
+    await axios
+      .get('/api/study/mystudy/like', config)
+      .then((response) => {
+        // console.log(response.data);
+        setLikes(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    userInfo();
+    likeList();
+  }, []);
   return (
     <Wrap>
-      <Profile />
+      <Profile
+        image={userDatas?.profile_image}
+        nickname={userDatas?.nickname}
+        introduce={userDatas?.introduce}
+        point={userDatas?.point}
+      />
       <Sidebar />
       <Box>
         <Item>
-          {dumyData.map((i) => (
-            <StudyCard key={i} />
+          {likes?.map((data) => (
+            <StudyCard
+              key={data.id}
+              id={data.id}
+              startDate={data.start_at}
+              people={data.limit_head_count}
+              title={data.title}
+              isOnline={data.is_online}
+              tag={data.StudyTags}
+              positon={data.position}
+              writer={data.User.nickname}
+              visit={data.visit_count}
+            />
           ))}
         </Item>
       </Box>
